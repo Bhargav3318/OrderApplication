cb89a3cfa54885ce4bb588763e596403308cadc6
Bhargav <bhargav3318@gmail.com>
2024-10-09 14:54:55 -0400

    added vscode and target folders to gitignore added a uuid OrderIdGenerator method for future use.

--- src/main/java/com/orderManagementSystem/dao/OrderIdGenerator.java
+++ src/main/java/com/orderManagementSystem/dao/OrderIdGenerator.java
@@ -1,12 +1,21 @@
 package com.orderManagementSystem.dao;
 
+import java.util.UUID;
+
 public class OrderIdGenerator {
 
     private static Long idSeed=1000L;
 
     public static Long generateNewOrderId(){
         
+        
         return ++idSeed;
     }
 
+    public static void generateUUIDBasedOrderID(){
+
+        String uuid = UUID.randomUUID().toString();
+
+
+    }
 }
